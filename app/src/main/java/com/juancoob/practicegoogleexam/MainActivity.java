bf197cefb09bfad18f6379e09f218014062bf79f
juancoob <ja.cobos.obrero@gmail.com>
2018-09-27 16:35:29 +0200

    feat: Added the floating action button logic to launch a new activity

--- app/src/main/java/com/juancoob/practicegoogleexam/MainActivity.java
+++ app/src/main/java/com/juancoob/practicegoogleexam/MainActivity.java
@@ -1,7 +1,11 @@
 package com.juancoob.practicegoogleexam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.fab_new_activity)
+    public void onClickNewActivityFloatingActionButton() {
+        Intent intentToDetailActivity = new Intent(this, DetailActivity.class);
+        startActivity(intentToDetailActivity);
+    }
+
 }
