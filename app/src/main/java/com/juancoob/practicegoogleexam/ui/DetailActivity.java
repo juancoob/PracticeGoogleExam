ec71488e3148f8b00a8300f08b53c5d22b599157
juancoob <ja.cobos.obrero@gmail.com>
2018-10-04 22:36:46 +0200

    feat: Updated Detail activity to show the increment count

--- app/src/main/java/com/juancoob/practicegoogleexam/ui/DetailActivity.java
+++ app/src/main/java/com/juancoob/practicegoogleexam/ui/DetailActivity.java
@@ -1,15 +1,54 @@
 package com.juancoob.practicegoogleexam.ui;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.juancoob.practicegoogleexam.R;
+import com.juancoob.practicegoogleexam.util.Constants;
 
-public class DetailActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @BindView(R.id.tv_counter)
+    public TextView counterTextView;
+
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setCounter();
+    }
+
+    private void setCounter() {
+        int value = mSharedPreferences.getInt(Constants.INCREMENT_KEY, Constants.DEFAULT_INCREMENT_VALUE);
+        counterTextView.setText(String.format(getString(R.string.counter), value));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(Constants.INCREMENT_KEY.equals(key)) {
+            setCounter();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
